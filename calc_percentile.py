17ba9196aee558d8ef9bb543ce0325d402efcc31
quppeq <quppeq@gmail.com>
2021-03-27 16:52:22 +0200

    add for

--- calc_percentile.py
+++ calc_percentile.py
@@ -1,7 +1,8 @@
 import pandas as pd
-
+from cannon import Cannon
 
 def calc_percentile(percentile, latency_list):
+    latency_list = list(filter(lambda x: x is not None, latency_list))
     select = percentile * len(latency_list)
     return sorted(latency_list)[int(select)]
 
@@ -11,13 +12,14 @@ def create_dataframe():
                                  "Latency, 50%", "Latency, 90%", "Latency, 99%", "Error rate"])
 
 
-def calculate_percentile(duration, max_qps, latency_list):
+def calculate_percentile(duration, max_qps, cannon: Cannon):
     df = create_dataframe()
-    for i in range(max_qps):
+    for i in range(100, max_qps, 10):
+        latency_list = cannon.start_shoot(max_qps, duration, threads=int(max_qps / 10))
         df.loc[i] = [duration,
                      duration*(i+1),
                      i+1,
-                     calc_percentile(.5, latency_list) ,
+                     calc_percentile(.5, latency_list),
                      calc_percentile(.9, latency_list),
                      calc_percentile(.99, latency_list),
                      100*latency_list.count(None)/len(latency_list)]
--- create_plots.py
+++ create_plots.py
@@ -10,10 +10,9 @@ if __name__ == '__main__':
     cannon = Cannon(URL)
 
     duration = 30
-    max_qps = 100
-    latency_list = cannon.start_shoot(max_qps, duration, threads=int(max_qps/10))
+    max_qps = 300
 
-    df = calculate_percentile(duration, max_qps, latency_list)
+    df = calculate_percentile(duration, max_qps, cannon)
 
     df.plot(x ="QPS(queries per second)", y="Latency, 50%", kind = 'line')
     df.plot(x ="QPS(queries per second)", y="Latency, 90%", kind = 'line')
